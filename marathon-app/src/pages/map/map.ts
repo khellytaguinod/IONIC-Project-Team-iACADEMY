b3fefbc73e778cf4cf06e0c5b7c01e0604e93a9b
khellytaguinod <khellytaguinod@gmail.com>
2018-01-19 14:27:57 +0800

    Added the Native-Map
    - working google native plugin

--- marathon-app/src/pages/map/map.ts
+++ marathon-app/src/pages/map/map.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  LatLng
+} from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
+
+
 /**
  * Generated class for the MapPage page.
  *
@@ -15,11 +28,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  map: GoogleMap;
+
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+    this.loadMap();
   }
 
+  loadMap() {
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+
+      let location = new LatLng(resp.coords.latitude, resp.coords.longitude)
+
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+          target: location,
+          zoom: 16
+          // tilt: 30
+        },
+        controls: {
+          compass: true,
+          myLocationButton: true,
+          indoorPicker: true,
+          zoom: true
+        }
+      };
+
+      this.map = GoogleMaps.create('map_canvas', mapOptions);
+      this.map.one(GoogleMapsEvent.MAP_READY)
+        .then(() => {
+          console.log('Map is ready!');
+        });
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+  }
 }
