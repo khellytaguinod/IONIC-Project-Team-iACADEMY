2346d06edfbf64b7f40f3236517f0b6a381a9612
Joanne Daroya <joannesimdaroya@gmail.com>
2018-01-19 09:02:28 +0800

    Update 1 - 1/19
    - added login, events, event-list, event, header components

--- marathon-web/src/app/app.module.ts
+++ marathon-web/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EventsComponent } from './core/events/events.component';
+import { HeaderComponent } from './core/header/header.component';
+import { EventListComponent } from './core/events/event-list/event-list.component';
+import { EventComponent } from './core/events/event-list/event/event.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EventsComponent,
+    HeaderComponent,
+    EventListComponent,
+    EventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
