b728b769db159413f1417751f24288837c8ecd8d
Joanne Daroya <joannesimdaroya@gmail.com>
2018-01-23 09:57:40 +0800

    new marathon-web
    * fixing errors.
    * temporary login page. fixing module errors (login feature ui only, no functionality yet)

--- marathon-web/src/app/core/header/header.component.ts
+++ marathon-web/src/app/core/header/header.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+}
