137b2066d453efd0602de2b7e8bb8b8d94ef8c7e
khellytaguinod <khellytaguinod@gmail.com>
2018-02-10 15:30:20 +0800

    Added a Map Route

--- marathon-admin/src/pages/event/event.ts
+++ marathon-admin/src/pages/event/event.ts
@@ -1,16 +1,23 @@
-import {Component} from '@angular/core';
-import {NavParams, NavController, AlertController, ActionSheetController, LoadingController, ToastController} from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavParams, NavController, AlertController, ActionSheetController, LoadingController, ToastController } from 'ionic-angular';
 import firebase from 'firebase';
 
-import {EditEventPage} from '../edit-event/edit-event';
+import { EditEventPage } from '../edit-event/edit-event';
 import { LiveEventPage } from '../live-event/live-event';
 import { EventsService } from '../../services/events';
+
+declare var google;
+
 
 @Component({
   selector: 'page-event',
   templateUrl: 'event.html',
 })
 export class EventPage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   eventData: any = {};
   event: string;
   participants: any = [];
@@ -21,7 +28,7 @@ export class EventPage {
   constructor(public navParams: NavParams, public navCtrl: NavController, private actionSheetCtrl: ActionSheetController, private eventsService: EventsService, public alertCtrl: AlertController, public loadCtrl: LoadingController, public toastCtrl: ToastController) {
     this.eventData = this.navParams.get('event');
     firebase.database().ref('participants/' + this.eventData.id).on('child_added', snapshot => {
-      if(snapshot) {
+      if (snapshot) {
         this.participants.push(snapshot.val());
       } else {
         this.participants = [];
@@ -37,6 +44,18 @@ export class EventPage {
 
   ionViewWillEnter() {
     this.event = 'details';
+    console.log('course map page');
+  }
+
+
+  ionViewDidEnter() {
+    this.loadMap()
+    this.showNavigation()
+  }
+
+  ionDidLoad(){
+    // this.loadMap();
+    // this.showNavigation();
   }
 
   onShowMore() {
@@ -45,7 +64,7 @@ export class EventPage {
         text: 'Edit',
         icon: 'create',
         handler: () => {
-          this.navCtrl.push(EditEventPage, {mode: 'edit', data: this.eventData});
+          this.navCtrl.push(EditEventPage, { mode: 'edit', data: this.eventData });
         }
       }, {
         text: 'Delete',
@@ -72,18 +91,18 @@ export class EventPage {
           load.present();
           let isDefault = (this.eventData.imgPath == this.default) ? true : false;
           this.eventsService.onDeleteEvent(this.eventData.id, isDefault)
-          .then(() => {
-            load.dismiss();
-            this.navCtrl.popToRoot();
-          })
-          .catch(err => {
-            load.dismiss();
-            let toast = this.toastCtrl.create({
-              message: 'Cannot delete event. Please try again.',
-              duration: 2000
-            });
-            toast.present();
-          })
+            .then(() => {
+              load.dismiss();
+              this.navCtrl.popToRoot();
+            })
+            .catch(err => {
+              load.dismiss();
+              let toast = this.toastCtrl.create({
+                message: 'Cannot delete event. Please try again.',
+                duration: 2000
+              });
+              toast.present();
+            })
         }
       }, {
         text: 'No'
@@ -94,10 +113,44 @@ export class EventPage {
 
   onLivePreview() {
     //Check database first if there is a currently live event
-    if(this.eventData.status != 'started') {
+    if (this.eventData.status != 'started') {
       //update database of event status = 'started'
     }
     this.navCtrl.setRoot(LiveEventPage);
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(12.8797, 121.7740); //PH map coordinates
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
+  showNavigation() {
+
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+
+    directionsDisplay.setMap(this.map);
+
+    directionsService.route({
+      origin: this.eventData.startPoint,
+      destination: this.eventData.endPoint,
+      travelMode: google.maps.TravelMode['WALKING']
+    }, (res, status) => {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(res);
+      } else {
+        console.warn(status);
+      }
+    });
+
   }
 
 }
