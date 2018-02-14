5dc92e86e4714a7891315c5a6807f1880d9c525b
Joanne Daroya <joannesimdaroya@gmail.com>
2018-02-14 18:09:53 +0800

    revise stats page

--- marathon-app/src/pages/stats/stats.ts
+++ marathon-app/src/pages/stats/stats.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
 import { LocationTrackerProvider } from '../../providers/location-tracker/location-tracker';
 import { EventPage } from '../event/event';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'page-stats',
   templateUrl: 'stats.html',
 })
 export class StatsPage {
+  name;
 
-  constructor(private navCtrl: NavController, private viewCtrl: ViewController, public locationTracker: LocationTrackerProvider) {}
+  constructor(private navCtrl: NavController, private viewCtrl: ViewController, public locationTracker: LocationTrackerProvider, private authService: AuthService) {}
+
+  ionViewWillEnter() {
+    this.authService.getUserDetails();
+    this.name = this.authService.username;
+  }
 
   onClose() {
     this.viewCtrl.dismiss();
