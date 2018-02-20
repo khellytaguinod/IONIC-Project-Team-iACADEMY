0e99422db4f42b0681c573319951b6da47c82d2e
James William M. Tarrobal <james.tarrobal.21@gmail.com>
2018-02-20 13:16:40 +0800

    Frequency when saving data to db

--- marathon-app/src/pages/map/map.ts
+++ marathon-app/src/pages/map/map.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { Http } from '@angular/http';
+import {Component, ViewChild} from '@angular/core';
+import {Http} from '@angular/http';
 
 import {ModalController, NavParams, Platform, AlertController, NavController, LoadingController} from 'ionic-angular';
 import {
@@ -12,12 +12,14 @@ import {
   Marker,
   LatLng
 } from '@ionic-native/google-maps';
-import { Geolocation } from '@ionic-native/geolocation';
-import { StatsPage } from '../stats/stats';
-import { LocationTrackerProvider } from '../../providers/location-tracker/location-tracker';
+import {Geolocation} from '@ionic-native/geolocation';
+import {StatsPage} from '../stats/stats';
+import {LocationTrackerProvider} from '../../providers/location-tracker/location-tracker';
 
 import parseTrack from 'parse-gpx/src/parseTrack'
 import xml2js from 'xml2js';
+import firebase from 'firebase';
+import {Storage} from "@ionic/storage";
 
 @Component({
   selector: 'page-map',
@@ -35,17 +37,19 @@ export class MapPage {
   map: GoogleMap;
   id;
   subscription;
+  frequency;
   loading;
+  userId = firebase.auth().currentUser.uid;
 
-  constructor(
-    private geolocation: Geolocation,
-    public locationTracker: LocationTrackerProvider,
-    private modalCtrl: ModalController,
-    private navParams: NavParams,
-    private platform: Platform,
-    private alertCtrl: AlertController,
-    public http: Http,
-    private loadCtrl: LoadingController) {
+  constructor(private geolocation: Geolocation,
+              public locationTracker: LocationTrackerProvider,
+              private modalCtrl: ModalController,
+              private navParams: NavParams,
+              private platform: Platform,
+              private alertCtrl: AlertController,
+              public http: Http,
+              private loadCtrl: LoadingController,
+              private storage: Storage) {
     this.platform.registerBackButtonAction(() => {
       let alert = this.alertCtrl.create({
         title: 'Are you sure you want to quit?',
@@ -67,11 +71,17 @@ export class MapPage {
     this.loading.present();
     this.id = this.navParams.get('id');
     this.name = this.navParams.get('event');
+    storage.get(this.userId).then(val => {
+      this.frequency = val;
+    })
   }
 
   ionViewDidEnter() {
     this.fetchGPX();
     this.onStart(this.id);
+    setInterval(() => {
+      this.locationTracker.saveToDatabase(this.id);
+    }, this.frequency)
   }
 
   fetchGPX() {
@@ -88,7 +98,7 @@ export class MapPage {
           for (let i = 0; i < this.gpxData.length; i++) {
             // console.log(track[i].latitude); // 43.512926660478115
             // console.log(track[i].longitude);
-            let coordinates = { lat: JSON.parse(this.gpxData[i].latitude), lng: JSON.parse(this.gpxData[i].longitude) };
+            let coordinates = {lat: JSON.parse(this.gpxData[i].latitude), lng: JSON.parse(this.gpxData[i].longitude)};
             this.list.push(coordinates);
           }
           console.log(this.list);
@@ -135,7 +145,7 @@ export class MapPage {
           'width': 7,
           'geodesic': false,
           'clickable': false
-        })
+        });
 
         this.map.addMarker({
           'position': this.list[0],
@@ -148,9 +158,9 @@ export class MapPage {
         }); // marker for end point
 
       }).catch((err) => {
-        alert('error loading course map')
-        console.log('Error setting up', err);
-      });
+      alert('error loading course map')
+      console.log('Error setting up', err);
+    });
     this.drawCurrentTrack()
   }
 
@@ -168,7 +178,7 @@ export class MapPage {
       .filter((p) => p.coords !== undefined) //Filter Out Errors
       .subscribe(position => {
         console.log(position.coords.longitude + ' ' + position.coords.latitude);
-        userTracks.push({ lat: position.coords.latitude, lng: position.coords.longitude })
+        userTracks.push({lat: position.coords.latitude, lng: position.coords.longitude})
 
         this.map.addPolyline({
           points: userTracks,
@@ -176,7 +186,7 @@ export class MapPage {
           'width': 6,
           'geodesic': false,
           'clickable': false
-        })
+        });
 
         this.map.setCameraTarget(userTracks.pop());
 
--- marathon-app/src/providers/location-tracker/location-tracker.ts
+++ marathon-app/src/providers/location-tracker/location-tracker.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, NgZone } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable, NgZone} from '@angular/core';
 
-import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import {Geolocation, Geoposition} from '@ionic-native/geolocation';
 import {
   BackgroundGeolocation,
   BackgroundGeolocationConfig,
@@ -9,23 +9,14 @@ import {
 } from '@ionic-native/background-geolocation';
 import 'rxjs/add/operator/filter';
 
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker,
-  LatLng
-} from '@ionic-native/google-maps';
+import {LatLng} from '@ionic-native/google-maps';
 import firebase from "firebase";
-import { Storage } from '@ionic/storage';
+import {Storage} from '@ionic/storage';
 
 @Injectable()
 export class LocationTrackerProvider {
 
-  public userTrack: any[] = [];
+  public userTrack: any = [];
   public list: any[] = [];  // james eto yung array na dapat isaved / push sa db natin
   // public list: any[] = [];  // james eto yung array na dapat isaved / push sa db natin
   public watch: any;
@@ -35,9 +26,10 @@ export class LocationTrackerProvider {
   userId = firebase.auth().currentUser.uid;
 
   constructor(public zone: NgZone,
-    private geolocation: Geolocation,
-    private backgroundGeolocation: BackgroundGeolocation,
-    private storage: Storage) { }
+              private geolocation: Geolocation,
+              private backgroundGeolocation: BackgroundGeolocation,
+              private storage: Storage) {
+  }
 
   startTracking(eventId) {
     // Background Tracking
@@ -56,8 +48,9 @@ export class LocationTrackerProvider {
         this.lat = location.latitude;
         this.lng = location.longitude;
 
-        let savedLocation = new LatLng(location.latitude, location.longitude)
-        this.userTrack.push(savedLocation);
+        let savedLocation = new LatLng(location.latitude, location.longitude);
+        let myKey = firebase.database().ref('userCoords/' + eventId + '/' + this.userId).push();
+        this.userTrack.push({[myKey.key]: savedLocation});
 
         // this.list.push(savedLocation); // james eto yung array na dapat isaved / push sa db natin
         // console.log(this.list);
@@ -73,7 +66,7 @@ export class LocationTrackerProvider {
     // Turn ON the background-geolocation system.
     this.backgroundGeolocation.start();
 
-    // Foreground Tracking
+    // // Foreground Tracking
     // let options = {
     //   frequency: 3000,
     //   enableHighAccuracy: true
@@ -81,33 +74,28 @@ export class LocationTrackerProvider {
 
     // this.watch = this.geolocation.watchPosition(options).filter((p: any) => p.code === undefined).subscribe((position: Geoposition) => {
     //   console.log(position);
-
-
-
-    //   // let myKey = firebase.database().ref('userCoords/' + eventId + '/' + this.userId).push().key;
-    //   // let savedCoordinates = new LatLng(position.coords.latitude, position.coords.longitude);
-    //   // firebase.database().ref('userCoords/' + eventId + '/' + this.userId + '/' + myKey).update({
-    //   //   lat: position.coords.latitude,
-    //   //   lng: position.coords.longitude
-    //   // });
-
-    //   // firebase.database().ref('/users/P9Ny6kCN13aVockO1qr7hXYLUsN2/coordinates/' + myKey).update(savedCoordinates);
-    //   // let savedCoordinates = new LatLng(position.coords.latitude, position.coords.longitude)
-    //   // this.list.push(savedCoordinates); // james eto yung array na dapat isaved / push sa db natin
-
-
     //   // Run update inside of Angular's zone
     //   this.zone.run(() => {
     //     this.lat = position.coords.latitude;
     //     this.lng = position.coords.longitude;
+    //
+    //     let savedLocation = new LatLng(position.coords.latitude, position.coords.longitude);
+    //     let myKey = firebase.database().ref('userCoords/' + eventId + '/' + this.userId).push();
+    //     this.userTrack.push({[myKey.key]: savedLocation});
     //   });
     // });
   }
 
-  stopTracking() {
+  saveToDatabase(eventId) {
+    this.userTrack.forEach(data => {
+      firebase.database().ref('userCoords/' + eventId + '/').child(this.userId).update(data);
+    });
+  }
 
+  stopTracking() {
     console.log('stopTracking');
     this.backgroundGeolocation.finish();
+    this.backgroundGeolocation.stop();
     // this.watch.unsubscribe();
     this.storage.set('coordinates', this.userTrack);
     console.log(' coordinates array saved ');
