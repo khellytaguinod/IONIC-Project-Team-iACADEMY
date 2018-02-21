5b656c8f863bc44222f6702c8011f2b7dc014173
Joanne Daroya <joannesimdaroya@gmail.com>
2018-02-21 16:29:36 +0800

    parsed xml

--- marathon-admin/src/pages/participants/participants.ts
+++ marathon-admin/src/pages/participants/participants.ts
@@ -13,6 +13,9 @@ export class ParticipantsPage implements OnInit {
   private offline;
   eventData;
   participants;
+  xml: string;
+  xmlLoop: string;
+  coords = [];
 
   constructor(public navParams: NavParams, public viewCtrl: ViewController, private navCtrl: NavController, private connectivity: ConnectivityService) {}
 
@@ -37,11 +40,31 @@ export class ParticipantsPage implements OnInit {
   }
 
   onFetchData(userId) {
-    console.log('Working');
-
-    firebase.database().ref('userCoords/' + this.eventData.id).child(userId).on('child_added', snapshot => {
-      console.log(snapshot.val());
-
+    new Promise((res,rej) => {
+      return firebase.database().ref('userCoords/' + this.eventData.id).child(userId).on('child_added', snapshot => {
+        if(snapshot) {
+          this.coords.push(snapshot.val());
+        }
+        res(this.coords);
+      });
+    }).then(data => {
+      let pointArr = data.map(point => {
+          return `<trkpt lat="${point.lat}" lon="${point.lng}">
+  <time>${point.time}</time>
+</trkpt>`;
+      });
+      this.xmlLoop = pointArr.reduce((aggregate, data) => aggregate + data);
+      this.xml = `<?xml version="1.0"?>
+<gpx version="1.0">
+  <trk>
+    <trkseg>
+    ${this.xmlLoop}
+    </trkseg>
+  </trk>
+</gpx>`;
+      let parser = new DOMParser();
+      let parsedXml = parser.parseFromString(this.xml, "application/xml");
+      console.log(parsedXml);
     })
   }
 }
